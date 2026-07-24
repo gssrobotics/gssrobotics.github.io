8ba366fbc01aafaf022093942ee5dd9368a2121b
Jarnu47 <arjun.subash@outlook.com>
2026-07-23 20:35:08 -0700

    Added responsive styles for mobile and tablet

--- script.js
+++ script.js
@@ -2,3 +2,7 @@ function goTo(id) {
     var element = document.getElementById(id);
     element.scrollIntoView({ behavior: "smooth" });
 }
+
+function toggleMobileMenu() {
+    document.querySelector('nav').classList.toggle("visible");
+}
